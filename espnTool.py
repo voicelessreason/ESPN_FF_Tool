9718e75167a58d680e45a0a2464fd00caa009014
Strausman <astrausm@c02t15lugtfl.ent.gartner.com>
2019-09-29 12:33:25 -0400

    Fix a typo and input my info.

--- espnTool.py
+++ espnTool.py
@@ -11,14 +11,14 @@ import signal
 
 # this should be usable across seasons, just be sure to update year and other info below
 year = 2019
-# change this value each wweek to ensure you are comparing the right teams
+# change this value each week to ensure you are comparing the right teams
 currentWeek = 4
 # input all league IDs separated by commas (required)
-leagues = [11111111, 22222222, 33333333]
+leagues = [487658, 1669270, 66999160]
 # optional
-leagueNames = ["The boiz", "Throwback", "New Gang"]
+leagueNames = ["Andrew League", "Bro League", "Work League"]
 # TEAM NAMES BELOW MUST MATCH YOUR TEAM NAME ON ESPN (required)
-teamNames = ["Quarter Chubb", "Hyde and Zeke", "As Far As the Eye Kelce"]
+teamNames = ["You're on Candid Kamara", "Hot Chubb Time Machine", "Slye Cooper"]
 
 ## Do not edit below this line if you don't know what you're doing
 ####################################################################
