e68882349c7203a331907c470061e70b432a3f4d
drippyer <parker.capron@gmail.com>
2019-10-07 13:02:14 -0500

    Change userData so user only inputs user/pass once

--- default_userData.py
+++ default_userData.py
@@ -1,29 +1,34 @@
-currentWeek = 5
+currentWeek = 5                         # Change this each week
 
-year = 2019
+year = 2019                             # Change this to reflect each season
+
+                                        # If any leagues are private
+espnUsername = "SampleUsername"         # input username here and
+espnPassword = "SamplePassword"         # input password here then
+                                        # modify variable names below
 
 league1 = [
-    "Sample League Name One", # League Name
-    11111111, # League ID
-    "Sample Team Name One", # Team Name (MUST BE CORRECT FOR YOUR TEAM)
-    "SampleUsername", # ESPN Username
-    "SamplePassword" # ESPN Password
+    "Sample League Name One",           # League Name
+    11111111,                           # League ID
+    "Sample Team Name One",             # Team Name (MUST BE CORRECT FOR YOUR TEAM)
+    espnUsername,                       # either (espnUsername) or (None)
+    espnPassword                        # either (espnPassword) or (None)
     ]
 
 league2 = [
-    "Sample League Name Two", # League Name
-    22222222, # League ID
-    "Sample Team Name Two", # Team Name (MUST BE CORRECT FOR YOUR TEAM)
-    None, # ESPN Username
-    None # ESPN Password
+    "Sample League Name Two",           # League Name
+    22222222,                           # League ID
+    "Sample Team Name Two",             # Team Name (MUST BE CORRECT FOR YOUR TEAM)
+    None,                               # either (espnUsername) or (None)
+    None                                # either (espnPassword) or (None)
     ]
 
 league3 = [
-    "Sample League Name Three", # League Name
-    33333333, # League ID
-    "Sample Team Name Three", # Team Name (MUST BE CORRECT FOR YOUR TEAM)
-    None, # ESPN Username
-    None # ESPN Password
+    "Sample League Name Three",         # League Name
+    33333333,                           # League ID
+    "Sample Team Name Three",           # Team Name (MUST BE CORRECT FOR YOUR TEAM)
+    None,                               # either (espnUsername) or (None)
+    None                                # either (espnPassword) or (None)
     ]
 
 leagues = [league1, league2, league3]
