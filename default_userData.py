df33b613ab2d14d7744e8a47626e80221dd3090b
voicelessreason <arlenstrausman@gmail.com>
2021-08-05 21:50:11 -0400

    Typo.

--- default_userData.py
+++ default_userData.py
@@ -5,7 +5,7 @@ year = 2021                             # Change this to reflect each season
 league1 = {
     "leagueName": "Sample League Name One",
     "leagueID": 11111111,
-    "teamName":  "Sample League Name One",
+    "teamName":  "Sample Team Name One",
     "espn_s2": '<ESPN_S2>',
     "swid": '<SWID>'
 }
@@ -13,7 +13,7 @@ league1 = {
 league2 = {
     "leagueName": "Sample League Name Two",
     "leagueID": 22222222,
-    "teamName":  "Sample League Name Two",
+    "teamName":  "Sample Team Name Two",
     "espn_s2": '<ESPN_S2>',
     "swid": '<SWID>'
 }
@@ -21,7 +21,7 @@ league2 = {
 league3 = {
     "leagueName": "Sample League Name Three",
     "leagueID": 33333333,
-    "teamName":  "Sample League Name Three",
+    "teamName":  "Sample Team Name Three",
     "espn_s2": '<ESPN_S2>',
     "swid": '<SWID>'
 }
